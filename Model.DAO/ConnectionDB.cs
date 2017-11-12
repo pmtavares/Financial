6daf96b127ff8a1378c81f762c7901f8cb90c88d
pmtavares <pedrotavares41@gmail.com>
2017-11-12 20:16:10 +0000

    Add project files.

---
+++ Model.DAO/ConnectionDB.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+
+namespace Model.DAO
+{
+    class ConnectionDB
+    {
+        private static ConnectionDB objConnectionDB = null;
+        private SqlConnection con;
+
+        //connection
+        private ConnectionDB()
+        {
+            con = new SqlConnection("Data Source=DESKTOP-LRS8LGG\\SQLEXPRESS; Initial Catalog=DBFinancial; Integrated Security=True");
+        }
+
+        public static ConnectionDB connectionStatus()
+        {
+            if(objConnectionDB == null)
+            {
+                objConnectionDB = new ConnectionDB();
+            }
+
+            return objConnectionDB;
+        }
+
+        public SqlConnection getCon()
+        {
+            return con;
+        }
+
+        public void closeDB()
+        {
+            objConnectionDB = null;
+        }
+
+    }
+}
